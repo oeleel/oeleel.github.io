af0eb1db5eb7f8528fc31f44227865c145732de6
Leo Lee <leosungwonlee@gmail.com>
2025-09-07 03:32:04 -0400

    Configure GitHub Pages via Actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/personal-website/', // <-- replace with your repo name; leave as '/' if repo is YOUR_USERNAME.github.io
+
 })
